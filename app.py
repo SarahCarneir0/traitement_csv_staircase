439a8577b863f472e42dff2ff5c31e515dcace62
yassbarona <yasserbarona@gmail.com>
2022-06-15 16:14:09 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,95 @@
+# import pckgs
+import streamlit as st
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+from statistics import mean
+
+
+st.title('TBD')
+st.text('Work hard... Play hard')
+
+data = st.file_uploader("Upload a Dataset", type=["csv"])
+
+
+if data is not None:
+    df = pd.read_csv(data)
+
+    l_values = df.loc[df['staircase_loop.intensity'].isna() == False, 'staircase_loop.intensity'].reset_index(drop=True)
+
+    def reversals(l_values):
+        l_reversals = []
+        for (el_before, el, el_after) in zip(l_values[:-2], l_values[1:-1], l_values[-2:]):
+            if (el_before > el and el_after > el) or (el_before < el and el_after < el):
+                l_reversals.append(el)
+        return l_reversals
+
+    # def reversals(l_values):
+    #     n = 1
+    #     state = 'down'
+    #     l_reversals = []
+    #     while n in range(len(l_values)):
+    #         if state == 'down':
+    #             if l_values[n] > l_values[n - 1]:
+    #                 state = 'up'
+    #                 l_reversals.append(l_values[n-1])
+    #             n += 1
+    #         elif state == 'up':
+    #             if l_values[n] < l_values[n - 1]:
+    #                 state = 'down'
+    #                 l_reversals.append(l_values[n-1])
+    #             n +=1
+    #     return l_reversals
+
+    with st.container():
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.subheader('List values')
+            st.write(l_values)
+
+        with col2:
+
+            st.subheader('Plot')
+            fig, ax = plt.subplots()
+            ax.plot(l_values.index.to_list(), l_values )
+            st.pyplot(fig)
+
+    with st.container():
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.subheader('List of reversal values')
+            new_df = pd.DataFrame(reversals(l_values))
+            st.write(new_df)
+
+        with col2:
+            st.subheader('Mean of last reversals')
+            st.write(f'Last two = {mean(reversals(l_values)[-2:])}')
+            st.write(f'Last three = {mean(reversals(l_values)[-3:])}')
+            st.write(f'Last four = {mean(reversals(l_values)[-4:])}')
+            
+    with st.container():
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            l_mistakes = df.loc[df['staircase_loop.response'].isna() == False, ['CorrectAns', 'staircase_loop.response']].reset_index(drop=True)
+            st.subheader('List Answers')
+            st.write(l_mistakes)
+
+        with col2:
+            st.subheader('Total incorrect answers')
+            st.write(l_mistakes[l_mistakes['staircase_loop.response'] == 0].value_counts())
+
+    with st.container():
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            total_t_trial = df['entrainement1.thisRepN'].loc[df['entrainement1.thisRepN'].isna() == False].count()
+            st.subheader('Total Training Trials')
+            st.title(total_t_trial)
+        
+        with col3:
+            total_s_trial = df['staircase_loop.thisTrialN'].loc[df['staircase_loop.thisTrialN'].isna() == False].count()
+            st.subheader('Total Staircase Trials')
+            st.title(total_s_trial)
